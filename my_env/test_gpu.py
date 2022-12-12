114a5af4827000d9a08a2e4725e5ca27c6e7380c
captain12321 <100767823+captain12321@users.noreply.github.com>
2022-12-12 19:57:42 +0800

    Delete test_gpu.py

--- my_env/test_gpu.py
+++
@@ -1,5 +0,0 @@
-import tensorflow as tf
-# 查看gpu和cpu的数量
-gpus = tf.config.experimental.list_physical_devices(device_type='GPU')
-cpus = tf.config.experimental.list_physical_devices(device_type='CPU')
-print(gpus, cpus)
